4242b91816d5a8810b867dcb3e4510c7aa97b0a2
sneh-shin <snehashinde800@gmail.com>
2022-11-24 22:15:27 +0530

    created TrackRepository interface that inherits MongoRepository

---
+++ src/main/java/com/niit/jap/repository/TrackRepository.java
@@ -0,0 +1,8 @@
+package com.niit.jap.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TrackRepository extends MongoRepository {
+}
