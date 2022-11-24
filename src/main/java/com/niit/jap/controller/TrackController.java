942fda452b173fe6604342dbd96308d025171683
sneh-shin <snehashinde800@gmail.com>
2022-11-24 22:43:27 +0530

    Autowired and injected TrackServiceImpl.java in the TrackController.java

--- src/main/java/com/niit/jap/controller/TrackController.java
+++ src/main/java/com/niit/jap/controller/TrackController.java
@@ -5,10 +5,16 @@
  */
 package com.niit.jap.controller;
 
+import com.niit.jap.service.TrackServiceImpl;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
 public class TrackController {
+    private TrackServiceImpl service;
+
+    public TrackController(TrackServiceImpl service) {
+        this.service = service;
+    }
 }
