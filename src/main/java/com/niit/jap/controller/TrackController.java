4fec830b4abdf21193a043b63d44383db0fd24f0
sneh-shin <snehashinde800@gmail.com>
2022-11-24 22:53:07 +0530

    added handler methods in the TrackController.java

--- src/main/java/com/niit/jap/controller/TrackController.java
+++ src/main/java/com/niit/jap/controller/TrackController.java
@@ -5,9 +5,11 @@
  */
 package com.niit.jap.controller;
 
+import com.niit.jap.domain.Track;
 import com.niit.jap.service.TrackServiceImpl;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -16,5 +18,30 @@ public class TrackController {
 
     public TrackController(TrackServiceImpl service) {
         this.service = service;
+    }
+
+    @PostMapping("/saveValue")
+    public ResponseEntity<?> save(Track track) {
+        return new ResponseEntity<>(service.saveTrack(track), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/getValue")
+    public ResponseEntity<?> getAll() {
+        return new ResponseEntity<>(service.getTracks(), HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping("/deleteValue")
+    public ResponseEntity<?> delete(int id) {
+        return new ResponseEntity<>(service.deleteTrack(id), HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("/getValue/{trackRating}")
+    public ResponseEntity<?> getByRating(double trackRating) {
+        return new ResponseEntity<>(service.getAllTrackFromTrackRating(trackRating), HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("/getValue/{artistName}")
+    public ResponseEntity<?> getByArtist(String artistName) {
+        return new ResponseEntity<>(service.getAllTrackFromArtistName(artistName), HttpStatus.ACCEPTED);
     }
 }
