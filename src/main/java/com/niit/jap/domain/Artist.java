efa3c25fa0eab82e3feae1a4ae76ff52afd166ce
sneh-shin <snehashinde800@gmail.com>
2022-11-24 22:12:06 +0530

    added constructors,getters and setters, and toString() in the Artist.java

--- src/main/java/com/niit/jap/domain/Artist.java
+++ src/main/java/com/niit/jap/domain/Artist.java
@@ -8,4 +8,36 @@ package com.niit.jap.domain;
 public class Artist {
     private int artistId;
     private String ArtistName;
+
+    public Artist() {
+    }
+
+    public Artist(int artistId, String artistName) {
+        this.artistId = artistId;
+        ArtistName = artistName;
+    }
+
+    public int getArtistId() {
+        return artistId;
+    }
+
+    public void setArtistId(int artistId) {
+        this.artistId = artistId;
+    }
+
+    public String getArtistName() {
+        return ArtistName;
+    }
+
+    public void setArtistName(String artistName) {
+        ArtistName = artistName;
+    }
+
+    @Override
+    public String toString() {
+        return "Artist{" +
+                "artistId=" + artistId +
+                ", ArtistName='" + ArtistName + '\'' +
+                '}';
+    }
 }
