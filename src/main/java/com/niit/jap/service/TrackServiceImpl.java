fea454ff13b50f7f0210581c4e781515eeb8589e
sneh-shin <snehashinde800@gmail.com>
2022-11-24 22:36:37 +0530

    implemented TrackService interface in the TrackServiceImpl.java

--- src/main/java/com/niit/jap/service/TrackServiceImpl.java
+++ src/main/java/com/niit/jap/service/TrackServiceImpl.java
@@ -5,8 +5,35 @@
  */
 package com.niit.jap.service;
 
+import com.niit.jap.domain.Track;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class TrackServiceImpl {
+public class TrackServiceImpl implements TrackService {
+    @Override
+    public Track saveTrack(Track track) {
+        return null;
+    }
+
+    @Override
+    public boolean deleteTrack(int trackId) {
+        return false;
+    }
+
+    @Override
+    public List<Track> getTracks() {
+        return null;
+    }
+
+    @Override
+    public List<Track> getAllTrackFromTrackRating(double trackRating) {
+        return null;
+    }
+
+    @Override
+    public List<Track> getAllTrackFromArtistName(String artistName) {
+        return null;
+    }
 }
