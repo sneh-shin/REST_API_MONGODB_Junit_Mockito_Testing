c4a635e763e163fbbc158d4e578819752ce1068b
sneh-shin <snehashinde800@gmail.com>
2022-11-24 22:28:31 +0530

    added userdefined methods in the TrackRepository interface

--- src/main/java/com/niit/jap/repository/TrackRepository.java
+++ src/main/java/com/niit/jap/repository/TrackRepository.java
@@ -1,8 +1,19 @@
 package com.niit.jap.repository;
 
+import com.niit.jap.domain.Track;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface TrackRepository extends MongoRepository {
+public interface TrackRepository extends MongoRepository<Track, Integer> {
+    @Query("{'trackRating':{$gt:[?0]}}")
+    List<Track> findAllTrackFromTrackRating(double trackRating);
+
+    @Query("{'trackArtist.artistName':{$in:[?0]}}")
+    List<Track> findAllTrackFromArtistName(String artistName);
+
 }
+
