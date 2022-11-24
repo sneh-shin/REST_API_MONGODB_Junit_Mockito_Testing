ae10083880a5b090b8939626e27dbbfc62881da3
sneh-shin <snehashinde800@gmail.com>
2022-11-24 22:38:12 +0530

    Autowired TrackRepository in the TrackServiceImpl.java

--- src/main/java/com/niit/jap/service/TrackServiceImpl.java
+++ src/main/java/com/niit/jap/service/TrackServiceImpl.java
@@ -6,12 +6,22 @@
 package com.niit.jap.service;
 
 import com.niit.jap.domain.Track;
+import com.niit.jap.repository.TrackRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class TrackServiceImpl implements TrackService {
+
+    private TrackRepository repository;
+
+    @Autowired
+    public TrackServiceImpl(TrackRepository repository) {
+        this.repository = repository;
+    }
+
     @Override
     public Track saveTrack(Track track) {
         return null;
