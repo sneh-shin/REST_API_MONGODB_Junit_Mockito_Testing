034209fb26eba0d70603e4e67b77a9da919363a6
sneh-shin <snehashinde800@gmail.com>
2022-11-24 22:42:30 +0530

    created TrackController.java in controller package and annotated with @RestController and @RequestMapping

---
+++ src/main/java/com/niit/jap/controller/TrackController.java
@@ -0,0 +1,14 @@
+/*
+ *Author Name: Sneha Shinde
+ *Date: 11/24/2022
+ * Created With: IntelliJ IDEA Community Edition
+ */
+package com.niit.jap.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class TrackController {
+}
