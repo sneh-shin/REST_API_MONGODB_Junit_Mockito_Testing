c9b01e936b7bddc55e2615b84208f8c3377e4663
sneh-shin <snehashinde800@gmail.com>
2022-11-25 15:39:08 +0530

    added annotation @ExtendsWith and @DataMongoTest in the TrackRepositoryTest.java

--- src/test/java/com/niit/jap/test/TrackRepositoryTest.java
+++ src/test/java/com/niit/jap/test/TrackRepositoryTest.java
@@ -5,5 +5,11 @@
  */
 package com.niit.jap.test;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@DataMongoTest
 public class TrackRepositoryTest {
 }
