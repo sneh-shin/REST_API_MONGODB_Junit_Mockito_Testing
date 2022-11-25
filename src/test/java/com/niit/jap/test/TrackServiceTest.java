3b490f1d2c95ddabd6a8a73fbf8ac876a6ba543c
sneh-shin <snehashinde800@gmail.com>
2022-11-25 15:14:54 +0530

    added @Mock and @InjectMocks annotations

--- src/test/java/com/niit/jap/test/TrackServiceTest.java
+++ src/test/java/com/niit/jap/test/TrackServiceTest.java
@@ -5,5 +5,17 @@
  */
 package com.niit.jap.test;
 
+import com.niit.jap.repository.TrackRepository;
+import com.niit.jap.service.TrackServiceImpl;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class TrackServiceTest {
+    @InjectMocks
+    TrackServiceImpl service;
+    @Mock
+    private TrackRepository repository;
 }
